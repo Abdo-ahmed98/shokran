5bd10e9af3ab5d7c4f805bde61de9d5d398b504c
Abdo-ahmed98 <mahmmed666600@gmail.com>
2025-01-10 17:12:13 +0200

    shokran

---
+++ main.js
@@ -0,0 +1,27 @@
+let get =document.getElementById("get")
+let gg =document.getElementById("gg")
+console.log(gg)
+function gget(){
+  scroll({
+    top:0,
+    behavior:"smooth"
+  })
+
+}
+let soii=document.querySelector(".soi")
+let but =document.getElementById("but")
+let li =document.querySelectorAll(".li")[0]
+
+let list = document.getElementById("list")
+function show(){
+  for(let i=0 ; i<li.length;i++){
+    console.log(li)
+
+  }
+  li.style.display="block"
+ 
+ 
+ 
+
+}
+console.log(li)
